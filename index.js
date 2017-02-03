19c3befe5d49afe06c315c5ac960322279891b4c
William P Thomsen <w@willbild.com>
2017-02-02 20:17:02 -0600

    feat(api): support official api

--- index.js
+++ index.js
@@ -4,16 +4,80 @@
   var _ = require('lodash');
   var requestPromise = require('request-promise');
   var bluebird = require('bluebird');
+  var Campaigns;
+  var Contacts;
+  var Lists;
+  var Reports;
+  var Website;
 
   exports.EmailOctopus = (function () {
-    function EmailOctopus(username, password) {
-      this.username = username;
-      this.password = password;
+    /**
+     * EmailOctopus API wrapper constructor
+     * @param {string} apiKey - Your EmailOctopus API key
+     * @param {string} [username] - Your EmailOctopus username (only needed if you want to create campaigns)
+     * @param {string} [password] - Your EmailOctopus password (only needed if you want to create campaigns)
+     * @constructor
+     */
+    function EmailOctopus(apiKey, username, password) {
+      this.apiKey = apiKey || process.env.EMAILOCTOPUS_APIKEY;
+      this.username = username || process.env.EMAILOCTOPUS_USERNAME;
+      this.password = password || process.env.EMAILOCTOPUS_PASSWORD;
       this.cookieJar = {};
+      this.campaigns = new Campaigns(this);
+      this.lists = new Lists(this);
+      this.website = new Website(this);
+      this.uriRoot = 'https://emailoctopus.com/api/1.1';
     }
 
     /**
-     * Create a campaign on Email Octopus. Mimics the website's campaign creation flow
+     * Sends request to EmailOctopus API
+     * @param {string} path
+     * @param {string} method
+     * @param {Object} [options]
+     * @returns {*}
+     * @private
+     */
+    EmailOctopus.prototype._apiRequest = function (path, method, options) {
+      var _this = this;
+
+      options = _.extend({api_key: _this.apiKey}, options || {});
+
+      return requestPromise({
+        uri: _this.uriRoot + path,
+        method: method,
+        json: true,
+        qs: method === 'GET' ? options : undefined,
+        formData: method !== 'GET' ? options : undefined
+      });
+    };
+
+    return EmailOctopus;
+  })();
+
+  Campaigns = (function () {
+    function Campaigns(master) {
+      this.master = master;
+      this.reports = new Reports(master);
+    }
+
+    /**
+     * https://emailoctopus.com/api-documentation/campaigns/get or
+     * https://emailoctopus.com/api-documentation/campaigns/get-all
+     * @param {string} [campaignId]
+     * @param {Object} [options]
+     * @param {number} [options.limit=100]
+     * @param {number} [options.page=1]
+     * @returns {*}
+     */
+    Campaigns.prototype.get = function (campaignId, options) {
+      var _this = this;
+      var path = '/campaigns' + (campaignId ? '/' + campaignId : '');
+
+      return _this.master._apiRequest(path, 'GET');
+    };
+
+    /**
+     * WARNING: Unofficial API add-on. Creates a campaign by mimicing the website's campaign creation flow
      * @param {Object} [options] - The options associated with the campaign
      * @param {string} [options.name] - The name of the campaign
      * @param {string} [options.subject] - The subject of the campaign
@@ -25,17 +89,17 @@
      * @param {string} bodyHtml - the entire html of the email
      * @returns {*}
      */
-    EmailOctopus.prototype.createCampaign = function (options, bodyHtml) {
+    Campaigns.prototype.create = function (options, bodyHtml) {
       var _this = this;
 
       return _this
-          ._signIn()
+          .master._signIn()
           .then(function () {
-            return _this._setupCampaign(options);
+            return _this._setup(options);
           })
           .then(function (campaignId) {
             return _this._selectTemplate(campaignId).then(function () {
-              return _this._designCampaign(campaignId, bodyHtml);
+              return _this._design(campaignId, bodyHtml);
             });
           })
           .finally(function () {
@@ -44,65 +108,16 @@
     };
 
     /**
-     * Mimic sign-in process on Email Octopus website
-     * @returns {*}
-     * @private
-     */
-    EmailOctopus.prototype._signIn = function () {
-      var _this = this;
-      var uri = 'https://emailoctopus.com/account/sign-in';
-
-      // Reset cookie jar
-      _this.cookieJar = requestPromise.jar();
-
-      return _this
-          ._getPageToken(uri, '_token')
-          .then(function (token) {
-            return requestPromise({
-              uri: uri,
-              method: 'POST',
-              jar: _this.cookieJar,
-              followAllRedirects: true,
-              headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-              },
-              formData: {
-                '_username': _this.username,
-                '_password': _this.password,
-                '_token': token,
-                '_remember_me': '1'
-              }
-            }).then(undefined, function (response) {
-              return bluebird.reject(new Error('sign in failed'));
-            });
-          });
-    };
-
-    /**
-     * Mimic sign-out process on Email Octopus website
-     * @returns {*}
-     * @private
-     */
-    EmailOctopus.prototype._signOut = function () {
-      var _this = this;
-
-      return requestPromise({
-        uri: 'https://emailoctopus.com/account/sign-out',
-        method: 'GET',
-        jar: _this.cookieJar
-      });
-    };
-
-    /**
      * Mimic the first step of creating a campaign (https://emailoctopus.com/campaigns/setup)
      * @param {Object} [options] - The options associated with the campaign
      * @returns {*}
      * @private
      */
-    EmailOctopus.prototype._setupCampaign = function (options) {
+    Campaigns.prototype._setup = function (options) {
       var _this = this;
       var uri = 'https://emailoctopus.com/campaigns/setup';
       var tokenInputName = 'campaign_setup[_token]';
+
       // Extend defaults with user-defined options
       var campaignSetup = _.extend({
         name: 'Campaign Name',
@@ -114,8 +129,15 @@
         toPersonalisationEnabled: 1
       }, options || {});
 
+      // Replace booleans with integers
+      _.each(campaignSetup, function (value, key) {
+        if (typeof value === 'boolean') {
+          campaignSetup[key] = value | 0;
+        }
+      });
+
       return _this
-          ._getPageToken(uri, tokenInputName)
+          .master.website._getPageToken(uri, tokenInputName)
           .then(function (token) {
             var formData = {};
 
@@ -143,19 +165,19 @@
     };
 
     /**
-     * Mimic template selection for a campaign (https://emailoctopus.com/campaigns/CAMPAIGN_ID/template)
+     * Mimic campaign template selection (https://emailoctopus.com/campaigns/CAMPAIGN_ID/template)
      * @param {string} campaignId
      * @returns {*}
      * @private
      */
-    EmailOctopus.prototype._selectTemplate = function (campaignId) {
+    Campaigns.prototype._selectTemplate = function (campaignId) {
       var _this = this;
       var uri = 'https://emailoctopus.com/campaigns/' + campaignId + '/template';
       var formData = {'campaign_template[template]': '3c3b6ab9-a0f7-11e6-b38e-080027632938'}; // "Totally Plain"
       var tokenInputName = 'campaign_template[_token]';
 
       return _this
-          ._getPageToken(uri, tokenInputName)
+          .master.website._getPageToken(uri, tokenInputName)
           .then(function (token) {
             formData[tokenInputName] = token;
             return requestPromise({
@@ -174,14 +196,14 @@
      * @returns {*}
      * @private
      */
-    EmailOctopus.prototype._designCampaign = function (campaignId, bodyHtml) {
+    Campaigns.prototype._design = function (campaignId, bodyHtml) {
       var _this = this;
       var uri = 'https://emailoctopus.com/campaigns/' + campaignId + '/design';
       var formData = {'campaign_design[bodyHtml]': bodyHtml};
       var tokenInputName = 'campaign_design[_token]';
 
       return _this
-          ._getPageToken(uri, tokenInputName)
+          .master.website._getPageToken(uri, tokenInputName)
           .then(function (token) {
             formData[tokenInputName] = token;
             return requestPromise({
@@ -194,6 +216,242 @@
           });
     };
 
+    return Campaigns;
+  })();
+
+  Contacts = (function () {
+    function Contacts(master) {
+      this.master = master;
+    }
+
+    /**
+     * https://emailoctopus.com/api-documentation/lists/get-contact or
+     * https://emailoctopus.com/api-documentation/lists/get-all-contacts
+     * @param {string} listId
+     * @param {string} [contactId]
+     * @param {Object} [options]
+     * @param {number} [options.limit=100]
+     * @param {number} [options.page=1]
+     * @returns {*}
+     */
+    Contacts.prototype.get = function (listId, contactId, options) {
+      var _this = this;
+      var path = '/lists/' + listId + '/contacts' + (contactId ? '/' + contactId : '');
+
+      return _this.master._apiRequest(path, 'GET');
+    };
+
+    /**
+     * https://emailoctopus.com/api-documentation/lists/create-contact
+     * @param {string} listId - The id of the list to add the contact to
+     * @param {Object} options
+     * @param {string} options.email_address - The email address of the contact
+     * @param {string} [options.first_name] - The first name of the contact
+     * @param {string} [options.last_name] - The last name of the contact
+     * @param {bool} [options.subscribed=true] - The initial subscribed status of the contact
+     * @returns {*}
+     */
+    Contacts.prototype.create = function (listId, options) {
+      var _this = this;
+      var path = '/lists/' + listId + '/contacts';
+
+      return _this.master._apiRequest(path, 'POST', options);
+    };
+
+    /**
+     * https://emailoctopus.com/api-documentation/lists/update-contact
+     * @param {string} listId
+     * @param {string} contactId
+     * @param {Object} options
+     * @param {string} [options.email_address] - The new email address for the contact
+     * @param {string} [options.first_name] - The new first name for the contact
+     * @param {string} [options.last_name] - The new last name for the contact
+     * @param {bool} [options.subscribed] - The new subscribed status for the contact
+     * @returns {*}
+     */
+    Contacts.prototype.update = function (listId, contactId, options) {
+      var _this = this;
+      var path = '/lists/' + listId + '/contacts/' + contactId;
+
+      return _this.master._apiRequest(path, 'PUT', options);
+    };
+
+    /**
+     * https://emailoctopus.com/api-documentation/lists/delete-contact
+     * @param {string} listId
+     * @param {string} contactId
+     * @returns {*}
+     */
+    Contacts.prototype['delete'] = function (listId, contactId) {
+      var _this = this;
+      var path = '/lists/' + listId + '/contacts' + contactId;
+
+      return _this.master._apiRequest(path, 'DELETE');
+    };
+
+    return Contacts;
+  })();
+
+  Lists = (function () {
+    function Lists(master) {
+      this.master = master;
+      this.contacts = new Contacts(master);
+    }
+
+    /**
+     * https://emailoctopus.com/api-documentation/lists/get or
+     * https://emailoctopus.com/api-documentation/lists/get-all
+     * @param {string} [listId]
+     * @param {Object} [options]
+     * @param {number} [options.limit=100] - Number of lists to return
+     * @param {number} [options.page=1] - Page to return records from
+     * @returns {*}
+     */
+    Lists.prototype.get = function (listId, options) {
+      var _this = this;
+      var path = '/lists' + (listId ? '/' + listId : '');
+
+      return _this.master._apiRequest(path, 'GET', options);
+    };
+
+    /**
+     * https://emailoctopus.com/api-documentation/lists/create
+     * @param {Object} options
+     * @param {string} options.name - The name of the new list
+     * @returns {*}
+     */
+    Lists.prototype.create = function (options) {
+      var _this = this;
+      var path = '/lists';
+
+      return _this.master._apiRequest(path, 'POST', options);
+    };
+
+    /**
+     * https://emailoctopus.com/api-documentation/lists/update
+     * @param {string} listId
+     * @param {Object} options
+     * @param {Object} options.name - The new name for the list
+     * @returns {*}
+     */
+    Lists.prototype.update = function (listId, options) {
+      var _this = this;
+      var path = '/lists/' + listId;
+
+      return _this.master._apiRequest(path, 'PUT', options);
+    };
+
+    /**
+     * https://emailoctopus.com/api-documentation/lists/delete
+     * @param {string} listId - The id of the list to delete
+     * @returns {*}
+     */
+    Lists.prototype['delete'] = function (listId) {
+      var _this = this;
+      var path = '/lists/' + listId;
+
+      return _this.master._apiRequest(path, 'DELETE');
+    };
+
+    return Lists;
+  })();
+
+  Reports = (function () {
+    function Reports(master) {
+      this.master = master;
+    }
+
+    var reportTypes = [
+      'summary',
+      'bounced',
+      'clicked',
+      'complained',
+      'opened',
+      'sent',
+      'unsubscribed',
+      'not-bounced',
+      'not-clicked',
+      'not-complained',
+      'not-opened',
+      'not-unsubscribed'
+    ];
+
+    // Batch generate methods for the various report endpoints
+    _.each(reportTypes, function (reportType) {
+      /**
+       * Retrieve a campaign report for a
+       * @param {string} campaignId
+       * @param {Object} [options]
+       * @param {number} [options.limit=100]
+       * @param {number} [options.page=1]
+       * @returns {*}
+       */
+      Reports.prototype[_.camelCase(reportType)] = function (campaignId, options) {
+        var _this = this;
+        var path = '/campaigns/' + campaignId + '/reports/' + reportType;
+
+        return _this.master._apiRequest(path, 'GET', options);
+      };
+    });
+
+    return Reports;
+  })();
+
+  Website = (function () {
+    function Website(master) {
+      this.master = master;
+    }
+
+    /**
+     * Mimic sign-in process on Email Octopus website
+     * @returns {*}
+     * @private
+     */
+    Website.prototype._signIn = function () {
+      var _this = this;
+      var uri = 'https://emailoctopus.com/account/sign-in';
+
+      // Reset cookie jar
+      _this.cookieJar = requestPromise.jar();
+
+      return _this
+          ._getPageToken(uri, '_token')
+          .then(function (token) {
+            return requestPromise({
+              uri: uri,
+              method: 'POST',
+              jar: _this.cookieJar,
+              followAllRedirects: true,
+              headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+              },
+              formData: {
+                '_username': _this.master.username,
+                '_password': _this.master.password,
+                '_token': token,
+                '_remember_me': '1'
+              }
+            }).then(undefined, function (response) {
+              return bluebird.reject(new Error('sign in failed'));
+            });
+          });
+    };
+
+    /**
+     * Mimic sign-out process on Email Octopus website
+     * @returns {*}
+     * @private
+     */
+    Website.prototype._signOut = function () {
+      var _this = this;
+
+      return requestPromise({
+        uri: 'https://emailoctopus.com/account/sign-out',
+        method: 'GET',
+        jar: _this.cookieJar
+      });
+    };
+
     /**
      * Fetch the token hidden input value on an Email Octopus html form
      * @param {string} uri - The uri of the Email Octopus form
@@ -201,7 +459,7 @@
      * @returns {*}
      * @private
      */
-    EmailOctopus.prototype._getPageToken = function (uri, tokenInputName) {
+    Website.prototype._getPageToken = function (uri, tokenInputName) {
       var _this = this;
 
       return requestPromise({
@@ -216,7 +474,6 @@
       });
     };
 
-    return EmailOctopus;
+    return Website;
   })();
-
 }).call(this);
